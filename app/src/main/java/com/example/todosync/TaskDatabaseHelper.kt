a699704fb829a3a5ba3f4c56a28f128e9f1cf5e7
Dinidi Gallage <dinidisg@gmail.com>
2024-05-11 20:56:58 +0530

    Implement Read task

--- app/src/main/java/com/example/todosync/TaskDatabaseHelper.kt
+++ app/src/main/java/com/example/todosync/TaskDatabaseHelper.kt
@@ -37,4 +37,24 @@ class TaskDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         db.close()
     }
 
+    fun getAllTasks(): List<Task>{
+        val taskList = mutableListOf<Task>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query, null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val task = Task(id, title, content)
+            taskList.add(task)
+        }
+
+        cursor.close()
+        db.close()
+        return taskList
+    }
+
 }
