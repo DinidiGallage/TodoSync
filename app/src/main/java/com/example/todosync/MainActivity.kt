acac1f3b2cef69563c1021f1f24e0e1493449216
Dinidi Gallage <dinidisg@gmail.com>
2024-05-12 08:23:10 +0530

    Read Tasks implementation

--- app/src/main/java/com/example/todosync/MainActivity.kt
+++ app/src/main/java/com/example/todosync/MainActivity.kt
@@ -3,19 +3,33 @@ package com.example.todosync
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todosync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TaskDatabaseHelper
+    private lateinit var tasksAdapter: TasksAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = TaskDatabaseHelper(this)
+        tasksAdapter = TasksAdapter(db.getAllTasks(), this)
+
+        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.tasksRecyclerView.adapter = tasksAdapter
+
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddTaskActivity::class.java) // Corrected Intent constructor
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tasksAdapter.refreshData(db.getAllTasks())
     }
 }
